0fe458197b3d49924f89091d0675abe54bbdf1d7
Daksh <dakshnahar2001@gmail.com>
2023-04-08 17:05:48 +0530

    final

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# Blog Website  with Comment and Connection Section.
+
+SignUp: You will be first directed to a signup page where you will enter you personal details to create a user.
+
+Home: After successful signup you will be directed to home page, where you will find all bogs listed there. there is also a navbar with  compose, connection and a home button for routing throught out the website.
+
+Compose: Here you can create a post with title and content.
+
+Post: When you are on home page, you can visit every post in detail, and you can comment on post.
+
+Connections: Here you will get your xth level connection list.
+
+TechStack: Node.js, Express.js, MongoDb, Ejs, etc.
+
+Functioning of Connections:
+-> Its a graph based algorithm
+-> Every person behaves as a vertice
+-> when a person post a comment on a blog, a edge is drawn between every other person who has commented on that post and the person
+-> Let's say shortest distance between User A and User B is 'x'. then UserA and UserB are a xth level friend.
+-> Shortest distace is calculated by BFS algorithm.
--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const ejs = require("ejs");
 const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
 
 const app = express();
 
@@ -17,20 +16,126 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
-
+// mongoose.connect("mongodb://localhost:27017/personDB", { useNewUrlParser: true });
 const postSchema = {
   title: String,
-  content: String
+  content: String,
+  comments_email:[String],
+  comments:[String],
 };
-
+const personSchema = {
+  firstName: String,
+  lastName: String,
+  email: String
+};
 const Post = mongoose.model("Post", postSchema);
+const Person = mongoose.model("Person", personSchema);
 
-app.get("/", function(req, res){
+class Graph {
+  constructor() {
+      this.vertices = [];
+      this.adjacent = {};
+      this.edges = 0;
+  }
+
+  addVertex(v) {
+      this.vertices.push(v);
+      this.adjacent[v] = [];
+  }
+
+  addEdge(v, w) {
+      this.adjacent[v].push(w);
+      this.adjacent[w].push(v);
+      this.edges++;
+  }
+
+  bfs(goal, root = this.vertices[0]) {
+    let adj = this.adjacent;
+
+    const queue = [];
+    queue.push(root);
+
+    const discovered = [];
+    discovered[root] = true;
+
+    const edges = [];
+    edges[root] = 0;
+
+    const predecessors = [];
+    predecessors[root] = null;
+
+    const buildPath = (goal, root, predecessors) => {
+        const stack = [];
+        stack.push(goal);
+
+        let u = predecessors[goal];
+
+        while(u != root) {
+            stack.push(u);
+            u = predecessors[u];
+        }
+
+        stack.push(root);
+
+        let path = stack.reverse().join('-');
+
+        return path;
+    }
+
+
+    while(queue.length) {
+        let v = queue.shift();
+
+        if (v === goal) {
+            return { 
+                distance: edges[goal],
+                path: buildPath(goal, root, predecessors)
+            };
+        }
+
+        for (let i = 0; i < adj[v].length; i++) {
+            if (!discovered[adj[v][i]]) {
+                discovered[adj[v][i]] = true;
+                queue.push(adj[v][i]);
+                edges[adj[v][i]] = edges[v] + 1;
+                predecessors[adj[v][i]] = v;
+            }
+        }
+    }
+
+    return false;
+}
+}
+
+
+g = new Graph();
+const allEmails = [];
+app.get("/", function (req, res) {
+  res.render("signup");
+});
+
+app.post("/", function (req,res) {
+  // const person = new Person({
+  //   firstName: req.body.fname,
+  //   lastName: req.body.lname,
+  //   email: req.body.email
+  // });
+  g.addVertex(req.body.email);
+  allEmails.push(req.body.email);
+  // person.save(function(err){
+  //   if (!err){
+  //       res.redirect("/home");
+  //   }
+  // });
+  res.redirect("/home");
+});
+
+app.get("/home", function(req, res){
 
   Post.find({}, function(err, posts){
     res.render("home", {
       startingContent: homeStartingContent,
-      posts: posts
+      posts: posts,
       });
   });
 });
@@ -48,7 +153,7 @@ app.post("/compose", function(req, res){
 
   post.save(function(err){
     if (!err){
-        res.redirect("/");
+        res.redirect("/home");
     }
   });
 });
@@ -60,18 +165,59 @@ const requestedPostId = req.params.postId;
   Post.findOne({_id: requestedPostId}, function(err, post){
     res.render("post", {
       title: post.title,
-      content: post.content
+      content: post.content,
+      PostId: requestedPostId 
     });
   });
 
 });
+app.post("/posts/:postId", function(req, res){
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+  const requestedPostId = req.params.postId;
+  const addedcomment = req.body.comment;
+  const personEmail = req.body.email;
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+    post.comments.push(addedcomment),
+    post.comments_email.push(personEmail);
+  });
+  
+  res.redirect("/home");
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+app.get("/connection/", function (req, res) {
+  const srcemail = allEmails[0];
+    Post.find({}, async function(err, posts){
+      let sz = posts.length;
+      for (var i = 0; i < sz; i++){
+        let sz2 = posts[i].comments_email.length;
+        for (var j = 0; j < sz2; j++){
+          for (var k = j + 1; k < sz2; k++){
+            if (posts[i].comments_email[j] != posts.comments_email[k]) {
+              console.log
+              g.add_edge(posts[i].comments_email[j], posts[i].comments_email[k]);
+            }
+          }
+        }
+      }
+      const res2 = [];
+      for (var i = 0; i < allEmails.length; i++){
+        if (srcemail != allEmails[i]) {
+          const x = g.bfs(srcemail, allEmails[i]);
+          if (x != false) {
+            res2.push({
+              distance: x.distance,
+              goal: allEmails[i]
+            })
+          }
+        }
+      }
+      // console.log(srcemail);
+      res.render("connection", {
+        res_arr: res2,
+        src: srcemail,
+      });
+
+    });
 });
 
 
